d66be559a8eb07213f843fcdd0e8583f18724114
AdamBrewington <adambrewington33@gmail.com>
2026-04-11 10:05:47 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(cached => {
       const fetchPromise = fetch(event.request).then(response => {
-        if (response.ok) {
+        if (response.ok && event.request.method === 'GET') {
           const clone = response.clone();
           caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
         }
